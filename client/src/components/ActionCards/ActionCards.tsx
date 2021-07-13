a0e6801babbe888369637ac99c7928e2596454b8
upluke <lukeforadream@gmail.com>
2021-07-13 00:03:15 -0700

    directories setup

---
+++ client/src/components/ActionCards/ActionCards.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ActionCards: React.FC=()=>{
+    return(
+        <h1>ActionCards</h1> 
+    )
+}
+
+export default ActionCards
---
+++ client/src/components/UserChecklist/UserChecklist.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserChecklist: React.FC=()=>{
+    return(
+        <h1>UserChecklist</h1> 
+    )
+}
+
+export default UserChecklist

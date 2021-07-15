3448210b7f1f82c911eb2358872661cc48d8905a
upluke <lukeforadream@gmail.com>
2021-07-15 00:03:28 -0700

    connected with type error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,73 @@
 import React from 'react';
- 
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
+
+// data fetch testing
+// client
+//   .query({
+//     query: gql`
+//       query GetTodo{
+//         todo{
+//           title
+//           completed
+//         }
+//       }
+//   `
+// })
+// .then(result=>console.log(result))
+interface TodoType {
+  title: string;
+  completed: boolean;
+}
+interface TodoTypeData {
+  todoType: TodoType[];
+}
+ 
+
+const TODOS=gql`
+    query GetTodo{
+      todo{
+        title
+        completed
+      }
+    }
+`
+
+// function Display(){
+//   const{loading, error, data}=useQuery <TodoTypeData> (TODOS)
+//   if(loading) return <p>Laoding....</p>
+//   if(error) return <p>Error occurs</p>
+   
+//   console.log(data)
+//   return data?.todo.map(({ title, completed }) => (
+//     <div key={title}>
+//       <p>
+//         {title}: {completed}
+//       </p>
+//     </div>
+//   ))
+
+// }
 
 function App() {
   return (
-    <div className="App">
-        <h1>Lou Team</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+          <h1>Lou Team</h1>
+           
+      </div>
+    </ApolloProvider>
   );
 }
 

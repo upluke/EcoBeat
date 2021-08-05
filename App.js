4bfbbf631c890f591c8174958c0d75db531e4590
Alex Simion <48101844+AlexSimion05@users.noreply.github.com>
2021-08-05 15:16:16 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React from "react";
+import Aboutpage  from './about';
+import Loginpage from './login';
+
+function App(props) {
+  return (<Loginpage/>,<Aboutpage/>)
+};
+
+export default App;

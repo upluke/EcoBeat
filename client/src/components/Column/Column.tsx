c7a630422d563c0f7ad20edb66d9455fffa698ed
Pranav <55062649+nurupranav@users.noreply.github.com>
2021-08-05 19:19:48 -0700

    Updated DND Playground (DNDPage) with the styling of the current "HomePage" The current "HomePage" should later be replaced with DNDPage.
    Missing features:
    -Help Message is static.
    
    Previously, I had it passed in as a prop with each ActionList,
    but 'Columns' are set up differently, so that will take a different approach.
    
    Also tried to add the close button to the helpDisplay, but will complete that in a later commit.

--- client/src/components/Column/Column.tsx
+++ client/src/components/Column/Column.tsx
@@ -3,24 +3,21 @@ import { Droppable } from "react-beautiful-dnd";
 import ColumnItem from "../ColumnItem";
 import { makeStyles } from "@material-ui/core/styles";
 
-
-interface ColumnInterface {
-  col:{
-  columnName:string;
-  orderedActions: string[];
-}
-}
+import React from 'react'
+import Paper from '@material-ui/core/Paper';
+import HelpDisplay from '../../components/HelpDisplay';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   column: {
-      display: "flex",
-      flexDirection:"column",
-      marginTop:8,
-      padding: '24px 0',
+    display: "flex",
+    flexDirection: "column",
+    marginTop: 8,
+    padding: '24px 0',
 
-    
+
   },
-  list:{
+  list: {
     backgroundColor: "#DAFEE2",
     borderRadius: 8,
     padding: 16,
@@ -29,41 +26,86 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     marginTop: 8
   },
-  title:{
-    padding: "0 1rem",
-    margin:"2rem 0",
-    textAlign:"center",
-    color:"white",
-    
-  }
+  title: {
+    // padding: "0 1rem",
+    // margin: "2rem 0",
+    // textAlign: "center",
+    // color: "white",
+
+    textAlign: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '8em',
+    fontSize: '1.5em',
+
+  },
+  brown_paper: {
+    padding: theme.spacing(2.3),
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(.5),
+    minHeight: '6vw',
+    textAlign: 'center',
+    backgroundColor: 'rgba(164,91,91,.53)',
+    borderRadius: '0px 0px 40px 40px',
+  },
+  green_paper: {
+    square: false,
+    backgroundColor: 'rgba(218,254,226)',
+    borderRadius: '40px 40px 0px 0px',
+    padding: theme.spacing(3, 2),
+    height: '1.5vw',
+    textAlign: 'center',
+    verticalAlign: 'middle',
+  },
+  add_button: {
+    backgroundColor: '#2CF9AC',
+    width: '100%',
+  },
 }));
 
 
+interface ColumnInterface {
+  col: {
+    columnName: string;
+    orderedActions: string[];
+  }
+}
 
+function capitalizeFirstLetter(text: string) {
+  // From https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
 
-const Column: React.FC<ColumnInterface> = ({col:{orderedActions,columnName }}) => {
-  const capColumnName=columnName.toUpperCase()
+const Column: React.FC<ColumnInterface> = ({ col: { orderedActions, columnName } }) => {
+  // const capColumnName = columnName.toUpperCase()
   const classes = useStyles();
-    return (
+  return (
+    <div>
+
+      <div style={{ float: 'right', paddingTop: '10px' }}>
+        <HelpDisplay helpMessage={'hello'} />
+      </div>
+
+      <Paper className={classes.green_paper} elevation={20} >
+        <Typography className={classes.title}> {capitalizeFirstLetter(columnName)} </Typography>
+      </Paper>
+
       <Droppable droppableId={columnName}>
         {(provided) => (
-          <div className={classes.column}>
-            <h2 className={classes.title}>{capColumnName}</h2>
-            <div 
-             className={classes.list}   
-             {...provided.droppableProps} ref={provided.innerRef}>
-     
-              {orderedActions&&orderedActions.map((orderedAction, index) => (
-            
+          <div >
+            <Paper className={classes.brown_paper}
+              {...provided.droppableProps} ref={provided.innerRef}>
+
+              {orderedActions && orderedActions.map((orderedAction, index) => (
                 <ColumnItem key={orderedAction} text={orderedAction} index={index} />
-             
               ))}
               {provided.placeholder}
-            </div>
+            </Paper>
           </div>
         )}
       </Droppable>
-    );
-  };
-  
-  export default Column;
+    </div>
+  );
+};
+
+export default Column;
--- client/src/pages/DNDPage/DNDPage.tsx
+++ client/src/pages/DNDPage/DNDPage.tsx
@@ -1,43 +1,40 @@
 import React from 'react'
-
-import { Paper } from '@material-ui/core';
-import { makeStyles } from "@material-ui/core/styles";
-
+import NavBar from '../../components/NavBar';
 import ColumnList from '../../components/ColumnList';
+// import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexGrow: 2,
-    padding: 0,
-    margin: 0,
-    height: "40%",//100vh
-    backgroundColor: "#113537",
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: 'flex',
+//     flexGrow: 1,
+//     // padding: 0,
+//     // margin: 0,
+//     // height: "40%",//100vh
+//     // backgroundColor: "#113537",
 
-  },
-  grid: {
-    marginTop: "1rem"
-  },
-  list: {
-    width: '50%',
-    margin: '1rem',
-  },
-  dndPaper: {
-    width: "100%",
-    backgroundColor: "red",
+//   },
+//   grid: {
+//     marginTop: "1rem"
+//   },
+//   list: {
+//     width: '50%',
+//     margin: '1rem',
+//   },
+//   dndPaper: {
+//     width: "100%",
+//     backgroundColor: "red",
 
-  },
+//   },
 
-}));
+// }));
 
 const DNDPage: React.FC = () => {
-  const classes = useStyles();
+  // const classes = useStyles();
   return (
-      <Paper className={classes.dndPaper}>
-        <div >
-          <ColumnList />
-        </div>
-      </Paper>
+    <div >
+      <NavBar loggedIn={true} />
+      <ColumnList />
+    </div>
   )
 }
 

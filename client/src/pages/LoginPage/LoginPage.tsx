dce76b9817d7a7ccd8dafa05f1fe2c961f8fe098
AlexSimion05 <simi3050@mylaurier.ca>
2021-08-08 18:19:30 -0400

    Update LoginPage.tsx
    css style fixes

--- client/src/pages/LoginPage/LoginPage.tsx
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -24,6 +24,7 @@ const useSty = makeStyles((theme:Theme) => ({
         marginTop: '5%',
         color: '#EBEBEB',
         textAlign:'center',
+        display: 'flex'
     },
     cardField: {
         width: '40%',

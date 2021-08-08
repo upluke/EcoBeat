60a715af7eb96dba488552bea40446d1ba27aaf7
Pranav <55062649+nurupranav@users.noreply.github.com>
2021-08-08 10:20:39 -0700

    Code Formatter modified code spacing to make it look prettier. No actual changes. Just committing seperately so the actual changes are easy to see.

--- client/src/pages/CreatePage/CreatePage.tsx
+++ client/src/pages/CreatePage/CreatePage.tsx
@@ -6,41 +6,41 @@ import UserList from '../../components/UserList';
 import { Paper } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexGrow: 2,
-      padding: 0,
-      margin: 0,
-      height: "40%",//100vh
-      backgroundColor: "#113537",
-  
-    },
-    grid: {
-      marginTop: "1rem"
-    },
-    list: {
-      width: '50%',
-      margin: '1rem',
-    },
- 
-  
-  }));
-const AboutPage: React.FC=()=>{
-    const classes = useStyles();
-    return(
-      
-        <Paper className={classes.root}>
-        <div className={classes.list}>
-          <CreateUser />
-          <UserList />
-        </div>
-        <div className={classes.list}>
-          <CreateAction />
-          <ActionList />
-        </div>
+  root: {
+    display: 'flex',
+    flexGrow: 2,
+    padding: 0,
+    margin: 0,
+    height: "40%",//100vh
+    backgroundColor: "#113537",
 
-      </Paper>
-    )
+  },
+  grid: {
+    marginTop: "1rem"
+  },
+  list: {
+    width: '50%',
+    margin: '1rem',
+  },
+
+
+}));
+const AboutPage: React.FC = () => {
+  const classes = useStyles();
+  return (
+
+    <Paper className={classes.root}>
+      <div className={classes.list}>
+        <CreateUser />
+        <UserList />
+      </div>
+      <div className={classes.list}>
+        <CreateAction />
+        <ActionList />
+      </div>
+
+    </Paper>
+  )
 }
 
 export default AboutPage

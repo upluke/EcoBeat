6cf7e9b83656e0d506f85c98931093e44f74cff4
upluke <lukeforadream@gmail.com>
2021-08-23 23:04:00 -0700

    auth0history

---
+++ client/src/auth/auth0-provider-with-history.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+interface childrenType{
+    children:React.ReactNode
+}
+
+
+const Auth0ProviderWithHistory:React.FC<childrenType> = ({ children }) => {
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+  const history = useHistory();
+  
+  const onRedirectCallback = (appState:any) => {
+    history.push(appState?.returnTo || window.location.pathname);
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain!}
+      clientId={clientId!}
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+      audience={audience}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithHistory;
